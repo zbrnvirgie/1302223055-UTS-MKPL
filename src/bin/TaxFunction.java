26be6b712ac8f95ab41226426a1d63292b30d34d
zbrnvirgie <163149245+zbrnvirgie@users.noreply.github.com>
2025-04-19 03:32:43 +0700

    Update

--- src/bin/TaxFunction.java
+++ src/bin/TaxFunction.java
@@ -9,7 +9,7 @@ public class TaxFunction {
 
     public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
         if (numberOfMonthWorking > 12) {
-            System.err.println("Jumlah bulan bekerja lebih dari 12 per tahun");
+            System.err.println("More than 12 month working per year");
             return 0;
         }
 
