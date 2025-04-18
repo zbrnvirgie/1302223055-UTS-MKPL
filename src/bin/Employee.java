987d6775165398ededb6f954a243eb1669c42c6e
zbrnvirgie <163149245+zbrnvirgie@users.noreply.github.com>
2025-04-19 02:44:46 +0700

    Refactoring Data Clumps pada Employee

--- src/bin/Employee.java
+++ src/bin/Employee.java
@@ -12,11 +12,11 @@ public class Employee {
 	private String idNumber;
 	private String address;
 	
-	private LocalDate joinDate; ////tambahan
-	
+	private LocalDate joinDate;
 	private int monthWorkingInYear;
+
 	private boolean isForeigner;
-	private boolean gender;
+	private boolean gender; //true = Laki-laki, false = Perempuan
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -25,10 +25,10 @@ public class Employee {
 	private String spouseName;
 	private String spouseIdNumber;
 
-	private List<Child> children; //// tambahan
-	
+	private List<Child> children;
+
 	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address,
-					int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+			int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -37,10 +37,10 @@ public class Employee {
 		this.joinDate = LocalDate.of(yearJoined, monthJoined, dayJoined);
 		this.isForeigner = isForeigner;
 		this.gender = gender;
-		
+
 		children = new LinkedList<>();
 	}
-	
+
 	public void setMonthlySalary(int grade) {
 		if (grade == 1) {
 			monthlySalary = 3000000;
@@ -49,43 +49,43 @@ public class Employee {
 		} else if (grade == 3) {
 			monthlySalary = 7000000;
 		}
+
 		if (isForeigner) {
 			monthlySalary *= 1.5;
 		}
 	}
-	
-	public void setAnnualDeductible(int deductible) {	
+
+	public void setAnnualDeductible(int deductible) {
 		this.annualDeductible = deductible;
 	}
-	
-	public void setAdditionalIncome(int income) {	
+
+	public void setAdditionalIncome(int income) {
 		this.otherMonthlyIncome = income;
 	}
-	
+
 	public void setSpouse(String spouseName, String spouseIdNumber) {
 		this.spouseName = spouseName;
 		this.spouseIdNumber = spouseIdNumber;
 	}
-	
+
 	public void addChild(String childName, String childIdNumber) {
 		children.add(new Child(childName, childIdNumber));
 	}
-	
+
 	public int getAnnualIncomeTax() {
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == joinDate.getYear()) {
-			monthWorkingInYear = date.getMonthValue() - joinDate.getMonthValue();
+		LocalDate now = LocalDate.now();
+		if (now.getYear() == joinDate.getYear()) {
+			monthWorkingInYear = now.getMonthValue() - joinDate.getMonthValue();
 		} else {
 			monthWorkingInYear = 12;
 		}
-		
+
 		return TaxFunction.calculateTax(
 			monthlySalary,
 			otherMonthlyIncome,
 			monthWorkingInYear,
 			annualDeductible,
-			spouseIdNumber.equals(""),
+			spouseIdNumber == null || spouseIdNumber.isEmpty(),
 			children.size()
 		);
 	}
