08e711998f2f2c6e341e5e5dfa4b058d67037bdc
zbrnvirgie <163149245+zbrnvirgie@users.noreply.github.com>
2025-04-19 03:26:17 +0700

    Refactoring Primitive_Code-TaxFunction

--- src/bin/TaxFunction.java
+++ src/bin/TaxFunction.java
@@ -1,26 +1,32 @@
 package bin;
 
 public class TaxFunction {
+
+    private static final int PENGHASILAN_TIDAK_KENA_PAJAK = 54000000;
+    private static final int TAMBAHAN_PENGHASILAN_TIDAK_KENA_PAJAK_MENIKAH = 4500000;
+    private static final int TAMBAHAN_PENGHASILAN_TIDAK_KENA_PAJAK_ANAK = 1500000;
+    private static final double TARIF_PAJAK = 0.05;
+
     public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-   
         if (numberOfMonthWorking > 12) {
-            System.err.println("More than 12 month working per year");
+            System.err.println("Jumlah bulan bekerja lebih dari 12 per tahun");
+            return 0;
         }
-       
-        if (numberOfChildren > 3) {
-            numberOfChildren = 3;
-        }
-        
-        int annualIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
-        
-        int taxFreeIncome = 54000000;
+
+        int annualSalary = monthlySalary * numberOfMonthWorking;
+        int annualOtherIncome = otherMonthlyIncome * numberOfMonthWorking;
+        int totalIncome = annualSalary + annualOtherIncome - deductible;
+
+        int nonTaxableIncome = PENGHASILAN_TIDAK_KENA_PAJAK;
         if (isMarried) {
-            taxFreeIncome += 4500000;
-            taxFreeIncome += (numberOfChildren * 1500000); 
+            nonTaxableIncome += TAMBAHAN_PENGHASILAN_TIDAK_KENA_PAJAK_MENIKAH;
         }
-        
-        int tax = (int) Math.round(0.05 * (annualIncome - deductible - taxFreeIncome));
-        
-        return (tax < 0) ? 0 : tax;
+        nonTaxableIncome += Math.min(numberOfChildren, 3) * TAMBAHAN_PENGHASILAN_TIDAK_KENA_PAJAK_ANAK;
+
+ 
+        int taxableIncome = totalIncome - nonTaxableIncome;
+        int tax = (int) Math.round(TARIF_PAJAK * Math.max(taxableIncome, 0));
+
+        return tax;
     }
 }
